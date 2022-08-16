2771a3507d0d8632b658eb67ab1c7d7192bf39d8
Namdeovibhor111 <lordvib3@gmail.com>
2022-08-16 17:33:39 +0530

    Add project files.

---
+++ MVCDb/Models/IDept.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDb.Models
+{
+	public interface IDept
+	{
+		List<Dept> GetDepts();
+		Dept FindDept(int id);
+		void AddDept(Dept dept);
+		void EditDept(Dept dept);
+		void DeleteDept(int id);
+
+	}
+}
