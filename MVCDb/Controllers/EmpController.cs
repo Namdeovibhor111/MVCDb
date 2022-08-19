7e7680beb2e6f08342ebd035e21fcab5c7ca7025
Namdeovibhor111 <lordvib3@gmail.com>
2022-08-19 15:31:15 +0530

    Employee all validations done

---
+++ MVCDb/Controllers/EmpController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVCDb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCDb.Controllers
+{
+    public class EmpController : Controller
+    {
+        DB1045Context db = new DB1045Context();
+
+        public IActionResult List()
+        {
+            var empdata = db.Emps.Include("Dept").ToList();
+            return View(empdata);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Deptid = new SelectList(db.Depts, "Id", "Name");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Emp emp)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Emps.Add(emp);
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+            ViewBag.Depid = new SelectList(db.Depts, "Id", "Name");
+            return View(emp);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var empdata = db.Emps.Find(id);
+            ViewBag.Deptid = new SelectList(db.Depts, "Id", "Name");
+            return View(empdata);
+        }
+        [HttpPost]
+        public IActionResult Edit(Emp emp)
+        {
+            if(ModelState.IsValid)
+            {
+                var odata = db.Emps.Find(emp.Id);
+                odata.Name = emp.Name;
+                odata.Salary = emp.Salary;
+                odata.Email = emp.Email;    
+                odata.Deptid = emp.Deptid;
+                odata.Dob = emp.Dob;
+                db.SaveChanges();
+                return RedirectToAction("List");
+            }
+            ViewBag.Deptid = new SelectList(db.Depts, "Id", "Name");
+            return View(emp);
+        }
+        public JsonResult EmailCheck(string Email)
+        {
+            bool yesno = db.Emps.Any(x => x.Email == Email);
+            return Json(!yesno);
+        }
+    }
+}
--- MVCDb/Models/Emp.cs
+++ MVCDb/Models/Emp.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -7,11 +9,22 @@ namespace MVCDb.Models
 {
     public partial class Emp
     {
+        [Display(Name = "Emp Code")]
         public int Id { get; set; }
+        [Display(Name = "Emp Name")]
+        [Required(ErrorMessage = "Name cannot be blank")]
         public string Name { get; set; }
+        [Display(Name = "Salary")]
+        [Range(10000, 90000, ErrorMessage = "Salary must be betweeen 10000 to 90000")]
         public int? Salary { get; set; }
+        [Display(Name ="Department ID")]
         public int? Deptid { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true,DataFormatString = "{0:yyyy-MM-dd}")]
+        [DOBCheck(ErrorMessage = "You must be atleast 25 year to work in L&T Info. ")]
         public DateTime? Dob { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [Remote("EmailCheck", "Emp", ErrorMessage = "Duplicate Email")]
         public string Email { get; set; }
 
         public virtual Dept Dept { get; set; }
